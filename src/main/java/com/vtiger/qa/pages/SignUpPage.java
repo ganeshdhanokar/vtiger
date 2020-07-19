c98f3f7a6e0af6bb8f7ffb16b808a263413894ed
surendrap86 <59413736+surendrap86@users.noreply.github.com>
2020-07-19 20:00:06 +0530

    SignUpPage Class

--- src/main/java/com/vtiger/qa/pages/SignUpPage.java
+++ src/main/java/com/vtiger/qa/pages/SignUpPage.java
@@ -11,7 +11,6 @@ public class SignUpPage extends TestBase{
 	@FindBy(xpath="//ul[@class='rd-navbar-nav']/li[1]/a")
 	WebElement clickSignUpLink;
 
-	
 	public void clickSignUpLink()
 	{
 		clickSignUpLink.click();
