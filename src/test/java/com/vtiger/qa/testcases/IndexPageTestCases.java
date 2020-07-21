3d80d3a3fb6b42f4d7990b0874b25df6342ed381
ganeshdhanokar <ganesh.dhanokar2009@gmail.com>
2020-07-21 10:48:24 +0530

    login link

--- src/test/java/com/vtiger/qa/testcases/IndexPageTestCases.java
+++ src/test/java/com/vtiger/qa/testcases/IndexPageTestCases.java
@@ -23,12 +23,12 @@ public class IndexPageTestCases extends TestBase {
 		indexPage = new IndexPage();
 	}
 	
-	@Test(priority=0)
+	@Test(priority=0, description="verifies title of index page")
 	public void verifyTitle() {
 		Assert.assertEquals(driver.getTitle(), "CRM Software | Customer Relationship Management System - Vtiger CRM");
 	}
 	
-	@Test(priority=1)
+	@Test(priority=1, description="verifies login link")
 	public void verifyLoginLink() {
 		LoginPage loginPage = indexPage.clickLoginLink();
 		Assert.assertEquals(driver.getTitle(), "Login - Vtiger");	
